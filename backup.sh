814da8cedc044c54d1ac9c5c5942d6b7b6737f3b
ysthakur <2004yst@gmail.com>
2022-02-20 17:09:05 +0000

    Add date properly to backup script

--- backup.sh
+++ backup.sh
@@ -2,5 +2,5 @@ cd /srv/robot-grav-site
 git config --local user.name blairrobotproject
 git config --local user.email blair.robot@gmail.com
 git add user/pages/*
-git commit -am "Backup for ${date}"
+git commit -am "Daily backup for $(date)"
 git push
